cbd8e84b896ea8ee175e1843f1607d82382f80f7
William Le <williamle8300@gmail.com>
2016-10-14 11:11:58 -0700

    asdf

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,15 @@
 var _ = require('lodash')
+var IsCurrentBillingMonth = require('is-same-monthyear')
+var Moment = require('moment')
+var MoneyMath = require('money-math')
 var React = require('react')
 var Request = require('superagent')
 
 var backend = require('../../config').backend
 
-var Invoices = require('./Invoices')
+var BillingHistory = require('./BillingHistory')
 var PushConfig = require('./PushConfig')
+var Modal = require('./Modal')
 
 
 module.exports = React.createClass({
@@ -20,8 +24,11 @@ module.exports = React.createClass({
 			email: '',
 			username: '',
 			password: '',
+			invoices: [],
+			modalVisible: false
 		}
 	},
+	dateNow: new Date(),
   render: function () {
 		
 		if (!this.props.user) return null
@@ -55,8 +62,8 @@ module.exports = React.createClass({
 						onChange={this.onChangePassword}/>
 				</div>
 				<button onClick={this.updateUser}>update</button>
-				<h2>Billing History</h2>
-				<Invoices jwt={this.props.jwt} user={this.props.user}/>
+				<h2>Usage for {Moment(this.dateNow).format('MMMM')} <small>@$0.04/notification</small></h2>
+				<this.CurrentUsage/>
 				<h2>Subscriptions</h2>
 				<PushConfig jwt={this.props.jwt} user={this.props.user} onUser={this.props.onUser}/>
 				<h2>Logout</h2>
@@ -68,6 +75,60 @@ module.exports = React.createClass({
 		if (!newProps.jwt) {
 			window.location = '/'
 		}
+	},
+	componentDidUpdate: function (prevProps, prevState) {
+		
+		if (!this.state.invoices.length && this.props.jwt && this.props.user) {
+			
+			this.readInvoices()
+		}
+	},
+	CurrentUsage: function () {
+		
+		var currentInvoice = this.state.invoices.filter((invoice) => {
+			return IsCurrentBillingMonth(new Date(invoice.creationDate))
+		})[0]
+		var overdueInvoices = this.state.invoices.filter((invoice) => {return !invoice.hasPaid && !IsCurrentBillingMonth(new Date(invoice.creationDate))})
+		var pastDue = (function() {
+			if (overdueInvoices.length === 0) return "0.00"
+			else if (overdueInvoices.length === 1)  return overdueInvoices[0].payable
+			else if (overdueInvoices.length > 1) return overdueInvoices.reduce((invoiceA, invoiceB) => {return MoneyMath.add(invoiceA.payable, invoiceB.payable)})
+		})()
+		
+		var dueDate = Moment(this.dateNow).endOf('month').format('Do')+ ' of ' +Moment(this.dateNow).format('MMMM')
+		
+		if (!currentInvoice) return null
+		
+		return (
+			<div>
+				<p>Current usage: <u>${currentInvoice.payable}</u></p>
+				<p>Pastdue usage: <u>${pastDue}</u></p>
+				<p>Payments are due end of month <i>({dueDate})</i></p>
+				<button onClick={() => {this.setState({modalVisible: true})}}>Billing history ({overdueInvoices.length})</button>
+				<Modal isVisible={this.state.modalVisible} onClose={this.closeModal} style={{}}>
+					<div onClick={(e) => e.stopPropagation()} style={{maxWidth: '50%'}}>
+						<button onClick={this.closeModal}>X</button>
+						<BillingHistory jwt={this.props.jwt} user={this.props.user}/>
+					</div>
+				</Modal>
+			</div>
+		)
+	},
+	closeModal: function () {
+
+		this.setState({modalVisible: false})
+	},
+	readInvoices: function () {
+		
+		Request
+		.get(backend+ '/user/' +this.props.user.id+ '/invoices')
+		.set({Authorization: 'Bearer ' +this.props.jwt})
+		.end((err, response) => {
+			
+			if (err) throw err
+			
+			return this.setState({invoices: response.body})
+		})
 	},
 	onChangeEmail: function (e) {
 		
