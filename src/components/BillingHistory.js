42f61a2ca8841c115e2e768ecf3d6ed089cfe3e8
William Le <williamle8300@gmail.com>
2016-10-14 11:11:54 -0700

    asdf

---
+++ src/components/BillingHistory.js
@@ -0,0 +1,58 @@
+var IsCurrentBillingMonth = require('is-same-monthyear')
+var Request = require('superagent')
+var React = require('react')
+
+var backend = require('../../config').backend
+
+
+module.exports = React.createClass({
+	propTypes: {
+		jwt: React.PropTypes.string,
+		user: React.PropTypes.object,
+	},
+	getInitialState: function () {
+		return {
+			invoices: []
+		}
+	},
+	render: function () {
+		return (
+			<div>
+				{
+					this.state.invoices
+					.filter((invoice) => {
+						return !IsCurrentBillingMonth(new Date(invoice.creationDate))
+					})
+					.map((invoice) => {
+						return (
+							<div key={invoice.id} style={{border: '1px solid black', backgroundColor: !invoice.hasPaid ? 'red' : 'none'}}>
+								<p>
+									Bill posted: {new Date(invoice.creationDate).toDateString()}
+									{
+										!invoice.hasPaid
+										? ' [OVERDUE]'
+										: null
+									}
+								</p>
+								<p>Paid: {invoice.hasPaid.toString()}</p>
+								<p>Due: ${invoice.payable}</p>
+							</div>
+						)
+					})
+				}
+			</div>
+		)
+	},
+	componentDidMount: function () {
+		
+		Request
+		.get(backend+ '/user/' +this.props.user.id+ '/invoices')
+		.set({Authorization: 'Bearer ' +this.props.jwt})
+		.end((err, response) => {
+			
+			if (err) throw err
+			
+			return this.setState({invoices: response.body})
+		})
+	}
+})
