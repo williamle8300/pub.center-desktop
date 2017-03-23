255101d7c6542adc60444fceab923f74ab39d829
William Le <williamle8300@gmail.com>
2017-03-22 18:00:30 -0700

    asdf

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
 var React = require('react')
 
+import MUIThemeable from 'material-ui/styles/muiThemeable'
 
-module.exports = React.createClass({
+
+module.exports = MUIThemeable()(React.createClass({
   render: function () {
     return (
-			<div>
-	    	<h1>PubCenter</h1>
-				<p>PubCenter is a non-profit organization specialized in building archives of historical records of RSS news data. Access to this data will always be completely free-of-charge to the public (no signup is required). We believe in the marketplace of ideas, and want to facilitate dialogue. We believe this can be accomplished simply by 1)archiving ideas chronologically and 2)making access free to everyone.</p>
-				---------
-				<p>give some examples of blogs they can instantly click to see it action</p>
-				---------
-				<p>If you would like support PubCenter, consider using our notifications services. We charge [prices/rulez/breakpoints/exampleimgs]</p>
-				-----------
+			<div style={this.style1()}>
+				<div style={{padding: this.props.muiTheme.spacing.desktopGutter}}>PubCenter is a non-profit organization specialized in building archives of historical records of RSS news data. Access to this data will always be completely free-of-charge to the public (no signup is required). We believe in the marketplace of ideas, and want to facilitate dialogue. We believe this can be accomplished simply by 1)archiving ideas chronologically and 2)making access free to everyone.</div>
 			</div>
     )
-  }
-})
+  },
+	style1: function () {
+		return {
+			height: '100vh',
+			background: this.props.muiTheme.palette.primary2Color,
+		}
+	},
+}))
